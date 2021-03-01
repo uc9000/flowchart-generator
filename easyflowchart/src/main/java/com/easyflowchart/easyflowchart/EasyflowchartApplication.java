469c3180dcd21bb24f08cae1410c7d0f32ec9cea
Jordan <jordanwiszniewski97@gmail.com>
2021-03-01 16:41:28 +0100

    first commit

---
+++ easyflowchart/src/main/java/com/easyflowchart/easyflowchart/EasyflowchartApplication.java
@@ -0,0 +1,13 @@
+package com.easyflowchart.easyflowchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyflowchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyflowchartApplication.class, args);
+	}
+
+}
---
+++ easyflowchart/src/test/java/com/easyflowchart/easyflowchart/EasyflowchartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyflowchart.easyflowchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyflowchartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
