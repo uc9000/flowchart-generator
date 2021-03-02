03b2313cc89323324aa8494d7463482493d78170
Jordan <jordanwiszniewski97@gmail.com>
2021-03-02 14:22:52 +0100

    mermaid graph basic implementation

--- easyflowchart/src/main/java/com/easyflowchart/models/FlowchartParser.java
+++ easyflowchart/src/main/java/com/easyflowchart/models/FlowchartParser.java
@@ -4,14 +4,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FlowchartParser extends FlowchartAttributes{
-
+    public final String GRAPH_DIRECTION;
     FlowchartParser(){
         setType("mermaid");
+        GRAPH_DIRECTION = new String("graph TD;\n"); // TD = top>down , LR = ledt>right etc.
+    }
+
+    private String handleMermaidCode(String input){
+        String output = new String(GRAPH_DIRECTION + input);
+        return output;
     }
 
     public String code2flowchart(String code){
         if(getType().equals("mermaid")){
-            return code;
+            return handleMermaidCode(code);
         }
         else if(getType().equals("C")){
             return "WIP";
