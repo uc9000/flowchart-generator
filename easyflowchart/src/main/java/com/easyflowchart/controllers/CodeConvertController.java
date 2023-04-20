bfbc2959a0895006b69a8b72ee92fe7bb37ffe91
Jordan Wiszniewski <jordanwiszniewski97@gmail.com>
2023-04-20 23:28:42 +0200

    add localhost:3000 to CORS

--- easyflowchart/src/main/java/com/easyflowchart/controllers/CodeConvertController.java
+++ easyflowchart/src/main/java/com/easyflowchart/controllers/CodeConvertController.java
@@ -6,12 +6,10 @@ import com.easyflowchart.enums.SyntaxType;
 import com.easyflowchart.models.FlowchartParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(
         value = "/api/v1",
         produces = "application/json",
