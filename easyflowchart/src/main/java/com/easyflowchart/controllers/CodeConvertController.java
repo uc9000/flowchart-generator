4111356d9ea6f84023a2d6ba9dd3dd71b2eccf14
Jordan Wiszniewski <jordanwiszniewski97@gmail.com>
2023-05-11 21:46:39 +0200

    add railway backend to CORS

--- easyflowchart/src/main/java/com/easyflowchart/controllers/CodeConvertController.java
+++ easyflowchart/src/main/java/com/easyflowchart/controllers/CodeConvertController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "htps://easychart-production.up.railway.app"})
 @RequestMapping(
         value = "/api/v1",
         produces = "application/json",
